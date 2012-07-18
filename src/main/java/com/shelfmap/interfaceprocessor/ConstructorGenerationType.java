c908bc809f9c85518d836ab5e5172f9dbe92e003
Tsutomu YANO <t_yano@me.com>
2012-07-18 16:44:54 +0900

    Revert "Big renaming. Changed project name to classprocessor."
    This reverts commit ada38ee6c4c31033ffb61a9217332377c4effd6e.

---
+++ src/main/java/com/shelfmap/interfaceprocessor/ConstructorGenerationType.java
@@ -0,0 +1,11 @@
+package com.shelfmap.interfaceprocessor;
+
+/**
+ * enum for what constructor is generated now.
+ *
+ * @author Tsutomu YANO
+ */
+public enum ConstructorGenerationType {
+    FULL_ARG_CONSTRUCTOR,
+    READ_ONLY_CONSTRUCTOR;
+}
